54521f0ac77553dbf84c2b9e3dd3683deaddcc56
Chakradhar Bogi <chakradharbogi@gmail.com>
2026-06-23 09:53:34 +0530

    Added worker profile route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -182,5 +182,36 @@ router.put("/rate/:userId", async (req, res) => {
   }
 
 });
+// ====================================
+// GET USER PROFILE
+// ====================================
+router.get("/:userId", async (req, res) => {
 
+  try {
+
+    const user = await User.findById(
+      req.params.userId
+    );
+
+    if (!user) {
+
+      return res.status(404).json({
+        message: "User not found"
+      });
+
+    }
+
+    res.status(200).json(user);
+
+  }
+
+  catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    });
+
+  }
+
+});
 module.exports = router;
